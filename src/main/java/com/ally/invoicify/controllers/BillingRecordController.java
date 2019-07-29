7d53719fdd88474e90af8bafbcaf042b4e371ae1
Mattias Ljung <mattias.ljung@galvanize.com>
2019-07-29 09:20:23 -0400

    init

---
+++ src/main/java/com/ally/invoicify/controllers/BillingRecordController.java
@@ -0,0 +1,31 @@
+package com.ally.invoicify.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+import com.ally.invoicify.models.BillingRecord;
+import com.ally.invoicify.repositories.BillingRecordRepository;
+import com.ally.invoicify.repositories.CompanyRepository;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/billing-record")
+public class BillingRecordController {
+	
+	private BillingRecordRepository recordRepository;
+
+	public BillingRecordController(BillingRecordRepository recordRepository, CompanyRepository companyRepository) {
+		this.recordRepository = recordRepository;
+	}
+
+	@GetMapping
+	public List<BillingRecord> list() {
+		return recordRepository.findAll();
+	}
+	
+}
