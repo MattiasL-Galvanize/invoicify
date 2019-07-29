80c6e99b386b743b21e2bc8392798ba020bb0063
Mattias Ljung <mattias.ljung@galvanize.com>
2019-07-29 09:54:05 -0400

    generation type identity

--- src/main/java/com/ally/invoicify/models/Contact.java
+++ src/main/java/com/ally/invoicify/models/Contact.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 public class Contact{
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 
     @ManyToOne
